a61f14b5b009ebb42a335cd4ad5de7cbed7a7fe5
kot-mechanic <alexander@breev.pro>
2022-04-11 20:34:06 +0300

    фильтры в процессе

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask_httpauth import HTTPBasicAuth
 import time
 
 
-from products_api.model.models import db, Sales, Seller_services
+from model.models import db, Sales, Seller_services
 
 # from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://salesowner:salesowner@192.168.42.134/sales"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://salesowner:DESb05XQlEKWdkLGVGjMCNFLhE4oQF@95.165.131.159/sales"
 
 auth = HTTPBasicAuth()
 user = {'login': 'service', 'password': 'ScaNTestrApToWeITERaCEmanTALaver'}
@@ -130,6 +130,45 @@ def getsales():
         return {"sale": results}, 200
 
 
+@app.route('/sales/request', methods=['GET'])
+@auth.login_required
+def custom_request():
+    json = request.get_json()
+    user_id = json.get('user_id')
+    product_type = json.get('product_type')
+    product_id = json.get('product_id')
+    sale_id = json.get('sale_id')
+    # sales = Sales.query.filter_by(user_id=user_id, product_type=product_type, product_id=product_id).order_by(Sales.sale_id.desc()).limit(3)
+    if user_id is not None and product_type is not None and product_id is not None and sale_id is not None:
+        sales = Sales.query.filter(Sales.user_id==user_id, Sales.product_type==product_type, Sales.product_id==product_id, Sales.sale_id < sale_id).order_by(Sales.sale_id.desc()).limit(3)
+    if user_id is not None and product_type is not None and product_id is None and sale_id is not None:
+        sales = Sales.query.filter(Sales.user_id == user_id, Sales.product_type == product_type, Sales.sale_id < sale_id).order_by(Sales.sale_id.desc()).limit(3)
+    if user_id is not None and product_type is not None and product_id is not None and sale_id is None:
+        sales = Sales.query.filter(Sales.user_id==user_id, Sales.product_type==product_type, Sales.product_id==product_id).order_by(Sales.sale_id.desc()).limit(3)
+    if user_id is not None and product_type is None and product_id is not None and sale_id is not None:
+        sales = Sales.query.filter(Sales.user_id==user_id, Sales.product_id==product_id, Sales.sale_id < sale_id).order_by(Sales.sale_id.desc()).limit(3)
+    if user_id is  None and product_type is not None and product_id is not None and sale_id is not None:
+        sales = Sales.query.filter(Sales.product_type==product_type, Sales.product_id==product_id, Sales.sale_id < sale_id).order_by(Sales.sale_id.desc()).limit(3)
+
+# Получение информации по конкретной продаже
+    if request.method == 'GET':
+        results = [
+                {
+                "sale_id": sale.sale_id,
+                "seller_service_id": sale.seller_service_id,
+                "user_id": sale.user_id,
+                "product_id": sale.product_id,
+                "product_type": sale.product_type,
+                "rate": sale.rate,
+                "cost": sale.cost,
+                "promocode": sale.promocode,
+                "discount": sale.discount,
+                "payment_cost": sale.payment_cost,
+                "note": sale.note,
+                "date": sale.date
+                } for sale in sales]
+        return {"sale": results}, 200
+
 
 if __name__ == '__main__':
-    app.run(host='192.168.42.113', port=5000)
+    app.run(host='192.168.42.125', port=5000)
